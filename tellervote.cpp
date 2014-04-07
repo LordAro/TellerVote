572c4390148012224955d3e1a4d7511bdf5fbf20
LordAro <charlespigott@googlemail.com>
2014-04-08 00:23:29 +0100

    Make a command type and refactor the main command loop out

--- tellervote.cpp
+++ tellervote.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 
@@ -7,6 +8,23 @@
 #include "tellervote.h"
 
 std::vector<Player> _players;
+
+Command StringToCommand(const std::string &str)
+{
+	auto found = std::find(COMMAND_MAP.begin(), COMMAND_MAP.end(), str);
+	if (found == COMMAND_MAP.end()) return Command_Length;
+	int dist = found - COMMAND_MAP.begin();
+	return static_cast<Command>(dist);
+}
+
+std::string CommandToString(const Command cmd)
+{
+	if(cmd < Command_Length) {
+		return COMMAND_MAP[cmd];
+	} else {
+		return nullptr;
+	}
+}
 
 void MakeMove(int self_id)
 {
@@ -26,6 +44,54 @@ void MakeMove(int self_id)
 	// Handle changes to hand in the played cmd
 }
 
+void CommandLoop(int self_id)
+{
+	std::vector<std::string> cmd = GetCommand();
+
+	/* No switch for strings :( */
+	switch (StringToCommand(cmd[0])) {
+		case Command_Reveal:
+			assert(cmd.size() == 3);
+			break;
+		case Command_Out:
+			assert(cmd.size() >= 2 && cmd.size() <= 4);
+			if (std::stoi(cmd[1]) == self_id) {
+				Debug() << "Oh noes, we lost!";
+				exit(0);
+			}
+			for (uint i = 2; i < cmd.size(); i++) {
+				// Remove cards from players possibilities
+			}
+			break;
+		case Command_Player:
+			assert(cmd.size() == 2);
+			if (std::stoi(cmd[1]) != self_id) break;
+
+			MakeMove(self_id);
+			break;
+		case Command_Played:
+			assert(cmd.size() >= 2 && cmd.size() <= 5);
+			// Remove played card (cmd[2]) from possibilities
+			break;
+		case Command_Protected:
+			assert(cmd.size() == 2);
+			Debug() << "Dammit, didn't realise player " << cmd[1] << " was protected";
+			break;
+		case Command_Swap:
+			assert(cmd.size() == 2);
+			break;
+		case Command_Discard:
+			assert(cmd.size() == 3);
+			break;
+		case Command_Ident:
+		case Command_Begin:
+		case Command_Draw:
+		default:
+			Debug() << "D: Unrecognised command! \"" << cmd[0] << "\"";
+			exit(1);
+	}
+}
+
 int main()
 {
 	std::vector<std::string> cmd = GetCommand();
@@ -43,39 +109,5 @@ int main()
 	cmd = GetCommand();
 	assert(cmd[0] == "begin");
 
-	for (;;) {
-		std::vector<std::string> cmd = GetCommand();
-
-		/* No switch for strings :( */
-		if (cmd[0] == "reveal") {
-			assert(cmd.size() == 3);
-		} else if (cmd[0] == "out") {
-			assert(cmd.size() >= 2 && cmd.size() <= 4);
-			if (std::stoi(cmd[1]) == self_id) {
-				Debug() << "Oh noes, we lost!";
-				exit(0);
-			}
-			for (uint i = 2; i < cmd.size(); i++) {
-				// Remove cards from players possibilities
-			}
-		} else if (cmd[0] == "player") {
-			assert(cmd.size() == 2);
-			if (std::stoi(cmd[1]) != self_id) continue;
-
-			MakeMove(self_id);
-		} else if (cmd[0] == "played") {
-			assert(cmd.size() >= 2 && cmd.size() <= 5);
-			// Remove played card (cmd[2]) from possibilities
-		} else if (cmd[0] == "protected") {
-			assert(cmd.size() == 2);
-			Debug() << "Dammit, didn't realise player " << cmd[1] << " was protected";
-		} else if (cmd[0] == "swap") {
-			assert(cmd.size() == 2);
-		} else if (cmd[0] == "discard") {
-			assert(cmd.size() == 3);
-		} else {
-			Debug() << "D: Unrecognised command! \"" << cmd[0] << "\"";
-			exit(1);
-		}
-	}
+	for (;;) CommandLoop(self_id);
 }
